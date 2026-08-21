480f6558d81ddb2e880b518949b16544caecdb2e
AnasMohammed77 <alsnwyansmhmdbdaljlyl@gmail.com>
2026-08-21 18:50:46 +0300

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -36,13 +36,14 @@ st.markdown("""
 col_1,col_2,col_3=st.columns([1,5,1])
 with col_2:
         st.image("imge.jpg")
-with st.container(border=True):
+
 
     st.markdown("""
     <div style="background:#9CA3AF;font-size:20px;padding:20px;border-radius:25px;">
     <div style="background:white;font-size:20px;padding:20px;border-radius:25px;margin:10px 0">
 
-    <h2> Project Overview</h2>
+    <h3> Project Overview</h3>
+    <i style="font-size:10px;">
     <p>          
             This project is an AI-based cybersecurity system designed to detect
             and classify cyber attacks by analyzing network traffic data using 
@@ -53,11 +54,13 @@ with st.container(border=True):
             traffic is normal or malicious. If an attack is detected, the system 
             identifies its type and provides the prediction confidence
     </p>  
+    </i>
     </div>    
                 
     <div style="background:white;font-size:20px;padding:20px;border-radius:25px;margin:10px 0">
 
-    <h2> Project Objectives<h2>
+    <h4> Project Objectives<h4>
+    <i style="font-size:10px;">
     <ul style="padding:6px;font-size:20px"> <i>            
                     <li> Detect cyber attacks.</li>
                     <li>Classify attack types.</li>
@@ -66,9 +69,10 @@ with st.container(border=True):
                     <li>Improve threat detection and response speed.</li>
                     </i>
                 </ul>
+    </i>
     </div>
     <div style="background:white;font-size:20px;padding:20px;border-radius:25px;margin:10px 0">
-
+     <i style="font-size:10px;">
     <h1> Technologies Used</h1>
     <h2> 🐍 Python</h2>
     <p>
@@ -95,17 +99,18 @@ with st.container(border=True):
     <p>
         Pandas is used for loading, cleaning, processing, and analyzing the dataset.
     </p>
+    </i>
     </div>
-    <div style="background:white;font-size:20px;padding:20px;border-radius:25px;margin:10px 0">
-    <div style="text-align:center;border-radius:10px;border:2px dashed;">
-    <h2 style="background:#00ffff;border-radius:25px;">Contact Me</h2>
-                        <p style="border-radius:70px;background:#00ffff;text-decoration:none;margin:5px 250px"><a href="https://mailto:alsnwyansmhmdbdaljlyl@gmail.com"> @Emial.com</a></p>
-                        <p style="border-radius:50px;background:#00ffff;text-decoration:none;margin:5px 250px"><i class="fa-brands fa-whatsapp"></i></i><a href="https://wa.me/967776713367">WhatsApp</a></a></p>
-                        <p style="border-radius:50px;background:#00ffff;text-decoration:none;margin:5px 250px"><i class="fa-brands fa-facebook-messenger"></i> <a href="https://m.me/ans.mhmd.alsnwy">Messenger</a></p>
-                        <p style="border-radius:50px;background:#00ffff;text-decoration:none;margin:5px 250px"><i class="fa-brands fa-facebook-f"></i><a href="https://www.facebook.com/share/19Fw8BRK99/">Facebook</a></p>
-                        <p style="border-radius:50px;background:#00ffff;text-decoration:none;margin:5px 250px"><i class="fab fa-telegram"></i> <a href="https://t.me/Anas_Alsanwy" target="_blank">Telegram</a></p>
+       <div style="text-align:center;border-radius:10px;border:2px dashed;">
+        <h2 style="background:#00ffff;border-radius:25px;">Contact Me</h2>
+                        <p style="border-radius:70px;background:#00ffff;text-decoration:none;"><a href="https://mailto:alsnwyansmhmdbdaljlyl@gmail.com"> @Emial.com</a></p>
+                        <p style="border-radius:50px;background:#00ffff;text-decoration:none;"><i class="fa-brands fa-whatsapp"></i></i><a href="https://wa.me/967776713367">WhatsApp</a></a></p>
+                        <p style="border-radius:50px;background:#00ffff;text-decoration:none;"><i class="fa-brands fa-facebook-messenger"></i> <a href="https://m.me/ans.mhmd.alsnwy">Messenger</a></p>
+                        <p style="border-radius:50px;background:#00ffff;text-decoration:none"><i class="fa-brands fa-facebook-f"></i><a href="https://www.facebook.com/share/19Fw8BRK99/">Facebook</a></p>
+                        <p style="border-radius:50px;background:#00ffff;text-decoration:none;"><i class="fab fa-telegram"></i> <a href="https://t.me/Anas_Alsanwy" target="_blank">Telegram</a></p>
         </div> 
-    </div>  
+        </i>
+        </div>
 
     <div style="background:white;border:2px dashed;margin:50px 0;padding:10px;border-radius:30px;text-algin:center">
     <h4> Developed by:</h4>
